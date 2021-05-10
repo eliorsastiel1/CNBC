c9c80ee00c8320033a2c2875f24688440c991cc0
Roie Cohen <roiecoh@gmail.com>
2021-05-10 16:31:27 +0300

    recovery 2

--- generate_suggestions.py
+++ generate_suggestions.py
@@ -4,6 +4,9 @@ import pickle
 from NER import ner
 from extract_companies_from_text import search_companies #uses fuzzywuzzy
 from fuzzywuzzy import process
+from tqdm import tqdm
+import multiprocessing
+import numpy as np
 #from dataWrapper.cnbc_data_loader import get_cnbc_data
 
 file_name =  os.path.join(os.path.dirname(__file__), 'Data/sentiment_data.pkl')
@@ -23,32 +26,64 @@ def sum_sentiment(row,suggestions,s):
         suggestions[row['Effective_Date']][s]={"Negative Count":suggestions[row['Effective_Date']][s]["Negative Count"]+1,"Negative Score":suggestions[row['Effective_Date']][s]["Negative Score"]+row['Sentiment_confidence'],"Positive Count":suggestions[row['Effective_Date']][s]["Positive Count"],"Positive Score":suggestions[row['Effective_Date']][s]["Positive Score"]}
     return suggestions
 
-#articles=get_cnbc_data()
-with open(file_name, 'rb') as in_file: 
-    cnbc_data = pickle.load(in_file)
-
-suggestions={}
-for index, row in cnbc_data.iterrows():
-    [entities,doc]=ner(row.content)
-    orginazations=[]
-    annotated=[]
-    for i,e in enumerate(entities):
-        if(e[1]=='ORG'):
-            ticker=search_companies(e[0],90)
-            if(ticker is not None):
-                orginazations.append(ticker[0])
-    orginazations = list(dict.fromkeys(orginazations))
-    #print(orginazations)
-    for s in orginazations:
-        if(row['Effective_Date'] in suggestions):
-            if(s in suggestions[row['Effective_Date']]):
-                suggestions=sum_sentiment(row,suggestions,s)
+def iterator(d):
+    suggestions={}
+    for index, row in d.iterrows():
+        [entities,doc]=ner(row.content)
+        orginazations=[]
+        for i,e in enumerate(entities):
+            if(e[1]=='ORG'):
+                ticker=search_companies(e[0],90)
+                if(ticker is not None):
+                    orginazations.append(ticker[0])
+        orginazations = list(dict.fromkeys(orginazations))
+        for s in orginazations:
+            if(row['Effective_Date'] in suggestions):
+                if(s in suggestions[row['Effective_Date']]):
+                    suggestions=sum_sentiment(row,suggestions,s)
+                else:
+                    suggestions=add_sentiment(row,suggestions,s)
             else:
+                suggestions[row['Effective_Date']]={}
                 suggestions=add_sentiment(row,suggestions,s)
+    return pd.DataFrame.from_dict({(i,j): suggestions[i][j] for i in suggestions.keys() for j in suggestions[i].keys()},orient='index')
+
+    
+if __name__ == '__main__':
+
+    with open(file_name, 'rb') as in_file: 
+        cnbc_data = pickle.load(in_file)
+
+    #cnbc_data=cnbc_data.iloc[0:5000,:]
+    print(cnbc_data.shape)
+    # create as many processes as there are CPUs on your machine
+    num_processes = multiprocessing.cpu_count()-2
+
+    # calculate the chunk size as an integer
+    chunk_size = int(cnbc_data.shape[0]/num_processes)
+    #print(chunk_size)
+    indices=[]
+    for partition in range(num_processes-1):
+        indices.append((partition+1)*chunk_size)
+    #print(indices)
+
+    #need to make sure we are not cutting within daetes for multiprocessing
+    idx=0
+    while idx<len(indices):
+        if(cnbc_data.iloc[indices[idx]]['Effective_Date'] == cnbc_data.iloc[indices[idx]-1]['Effective_Date'] ):
+            indices[idx]=indices[idx]+1
         else:
-            suggestions[row['Effective_Date']]={}
-            suggestions=add_sentiment(row,suggestions,s)
-with open(output, 'wb') as out_file:
-    pickle.dump(suggestions,out_file, protocol=-1)
+            idx=idx+1
+
+    print(indices)
+
+    df_split = np.array_split(cnbc_data, indices)
+    pool = multiprocessing.Pool(num_processes)
+    result = pd.concat(pool.map(iterator, df_split))
+
+#print(df_split.shape())
+#suggestions={}
+    with open(output, 'wb') as out_file:
+        pickle.dump(result,out_file, protocol=-1)
 
     
