82b4d6b49934bb7099aab4c2183dc4f015cbe54c
Roie Cohen <roiecoh@gmail.com>
2021-06-13 18:07:49 +0300

    cleanup

--- debug.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-from pandasgui import show
-val=pd.read_pickle('Data/0.03-0.03.pkl')
-show(val)
--- yfinance_test.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-msft=yf.Ticker("MSFT")
-hist=msft.history(period="max")
-plt.plot(hist)
-#print(msft.info)
-plt.show()
